9a067619dbda874414aba86c4c14161a568e65c8
MichaelMcKibbin <74902185+MichaelMcKibbin@users.noreply.github.com>
2024-08-01 00:08:50 +0100

    Refactor: Template list page; HTTP API calls centralized.

--- movies/src/pages/favoriteMoviesPage.js
+++ movies/src/pages/favoriteMoviesPage.js
@@ -1,7 +1,25 @@
-import React from "react";
+// import React from "react";
 
-const FavoriteMoviesPage = () => {
-  return <h2>Favorite Movies</h2>;
+// const FavoriteMoviesPage = () => {
+//   return <h2>Favorite Movies</h2>;
+// };
+
+// export default FavoriteMoviesPage;
+import React from "react";
+import PageTemplate from "../components/templateMovieListPage";
+
+const FavoriteMoviesPage = (props) => {
+  const toDo = () => true;
+  // Get movies from local storage.
+  const movies = JSON.parse(localStorage.getItem("favorites"));
+
+  return (
+    <PageTemplate
+      title="Favourite Movies"
+      movies={movies}
+      selectFavorite={toDo}
+    />
+  );
 };
 
 export default FavoriteMoviesPage;
