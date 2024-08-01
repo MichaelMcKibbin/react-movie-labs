17a81497abbd5bb5ddb867ec30793a186eaf5dcd
MichaelMcKibbin <74902185+MichaelMcKibbin@users.noreply.github.com>
2024-08-02 00:48:40 +0100

    Added upcoming movies page

---
+++ movies/src/pages/upcomingMoviesPage.js
@@ -0,0 +1,28 @@
+import React, { useState, useEffect } from "react";
+import PageTemplate from "../components/templateMovieListPage";
+
+const UpcomingMoviesPage = () => {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    fetch(
+      `https://api.themoviedb.org/3/movie/upcoming?api_key=${process.env.REACT_APP_API_KEY}&language=en-US&include_adult=false&page=1`
+    )
+      .then((res) => res.json())
+      .then((json) => {
+        return json.results;
+      })
+      .then((movies) => {
+        setMovies(movies);
+      });
+  }, []);
+
+  return (
+    <PageTemplate
+      title="Comming soon, to a theater near you..."
+      movies={movies}
+    />
+  );
+};
+
+export default UpcomingMoviesPage;
