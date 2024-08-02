787429a16d256d72c88e0e92328b61512cd72476
MichaelMcKibbin <74902185+MichaelMcKibbin@users.noreply.github.com>
2024-08-02 05:46:07 +0100

    Bug Fix - make action icons on movie card configurable.

--- movies/src/components/movieCard/index.js
+++ movies/src/components/movieCard/index.js
@@ -18,6 +18,7 @@ import { Link } from "react-router-dom";
 import Avatar from "@mui/material/Avatar";
 
 export default function MovieCard({ movie }) {
+  //export default function MovieCard({ movie, action }) {
   const { favorites, addToFavorites } = useContext(MoviesContext);
 
   if (favorites.find((id) => id === movie.id)) {
@@ -72,10 +73,18 @@ export default function MovieCard({ movie }) {
           </Grid>
         </Grid>
       </CardContent>
+
       <CardActions disableSpacing>
-        <IconButton aria-label="add to favorites" onClick={handleAddToFavorite}>
-          <FavoriteIcon color="primary" fontSize="large" />
-        </IconButton>
+        <Link to={`/movies/${movie.id}`}></Link>
+        {/*action(movie)*/}
+        {
+          <IconButton
+            aria-label="add to favorites"
+            onClick={handleAddToFavorite}
+          >
+            <FavoriteIcon color="primary" fontSize="large" />
+          </IconButton>
+        }
 
         <Link to={`/movies/${movie.id}`}>
           <Button variant="outlined" size="medium" color="primary">
@@ -86,3 +95,4 @@ export default function MovieCard({ movie }) {
     </Card>
   );
 }
+// CardActions caused problems. Look at it again later.
--- movies/src/pages/homePage.js
+++ movies/src/pages/homePage.js
@@ -1,45 +1,10 @@
-// import React, { useState, useEffect } from "react";
-// import PageTemplate from "../components/templateMovieListPage";
-
-// const HomePage = (props) => {
-//   const [movies, setMovies] = useState([]);
-//   const favorites = movies.filter((m) => m.favorite);
-//   localStorage.setItem("favorites", JSON.stringify(favorites));
-
-//   const addToFavorites = (movieId) => {
-//     const updatedMovies = movies.map((m) =>
-//       m.id === movieId ? { ...m, favorite: true } : m
-//     );
-//     setMovies(updatedMovies);
-//   };
-
-//   useEffect(() => {
-//     fetch(
-//       `https://api.themoviedb.org/3/discover/movie?api_key=${process.env.REACT_APP_API_KEY}&language=en-US&include_adult=false&page=1`
-//     )
-//       .then((res) => res.json())
-//       .then((json) => {
-//         return json.results;
-//       })
-//       .then((movies) => {
-//         setMovies(movies);
-//       });
-//   }, []);
-
-//   return (
-//     <PageTemplate
-//       title="Discover Movies"
-//       movies={movies}
-//       selectFavorite={addToFavorites}
-//     />
-//   );
-// };
-// export default HomePage;
 import React from "react";
 import { getMovies } from "../api/tmdb-api";
 import PageTemplate from "../components/templateMovieListPage";
 import { useQuery } from "react-query";
 import Spinner from "../components/spinner";
+import AddToFavoritesIcon from "../components/cardIcons/addToFavorites";
+import RemoveFromFavoritesIcon from "../components/cardIcons/removeFromFavorites";
 
 const HomePage = (props) => {
   const { data, error, isLoading, isError } = useQuery("discover", getMovies);
@@ -62,7 +27,9 @@ const HomePage = (props) => {
     <PageTemplate
       title="Discover Movies"
       movies={movies}
-      selectFavorite={addToFavorites}
+      action={(movie) => {
+        return <AddToFavoritesIcon movie={movie} />;
+      }}
     />
   );
 };
